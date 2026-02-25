d8a4a58357345fed4a79bc647a33b21c393c8d72
lucaspaiva-lp <gml248@tutamail.com>
2026-02-25 18:29:30 -0300

    wip: draft moneyball joins for performance queries

---
+++ 02-relating/moneyball/5.sql
@@ -0,0 +1,12 @@
+/* > It’s a bit of a slow day in the office. Though Satchel no longer plays, 
+    Write a SQL query to find all teams that Satchel Paige played for.
+        - Your query should return a table with a single column, one for the name of the teams.
+*/
+
+select *
+from performances
+join players
+    on performances.player_id = players.id
+join teams
+    on performances.team_id = teams.id
+where players.first_name = 'Satchel' and players.last_name = 'Paige';
