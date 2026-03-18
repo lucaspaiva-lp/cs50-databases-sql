2613359ec9edf51ff674e2246669e98b6cb20d1a
lk1048 <gml248@tutamail.com>
2026-03-18 20:03:10 -0300

    feat: new schema module

--- 2-designing/connect/schema.sql
+++ 2-designing/connect/schema.sql
@@ -3,20 +3,44 @@ CREATE TABLE users (
     first_name TEXT NOT NULL,
     last_name TEXT UNIQUE,
     username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
+    password TEXT NOT NULL
 );
+CREATE TABLE education (
+    user_id INTEGER,
+    school_id INTEGER,
+    degree TEXT,
+    start_date TEXT,
+    end_date TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (school_id) REFERENCES schools(id)
+);
+
 CREATE TABLE schools (
     id INTEGER PRIMARY KEY,
-    company_id INTEGER,
     name TEXT NOT NULL UNIQUE,
     type TEXT,
-    location REAL NOT NULL UNIQUE,
-    FOREIGN KEY (company_id) REFERENCES companies(id)
+    location TEXT NOT NULL,
+    founded_year TEXT
 );
 CREATE TABLE companies (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
-    type TEXT,
+    industry TEXT,
     location TEXT NOT NULL
-
+);
+CREATE TABLE employment (
+    user_id INTEGER,
+    company_id INTEGER,
+    title TEXT,
+    start_date TEXT,
+    end_date TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (company_id) REFERENCES companies(id)
+);
+CREATE TABLE connections (
+    user_id_1 INTEGER,
+    user_id_2 INTEGER,
+    PRIMARY KEY (user_id_1, user_id_2),
+    FOREIGN KEY (user_id_1) REFERENCES users(id),
+    FOREIGN KEY (user_id_2) REFERENCES users(id)
 );
